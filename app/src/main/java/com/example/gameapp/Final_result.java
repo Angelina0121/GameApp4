fa270da4fe299e150626fda6207ead3f71cc0904
Angelina <angelinabommarito@live.fr>
2020-04-17 18:07:47 +0200

    Fourth page

---
+++ app/src/main/java/com/example/gameapp/Final_result.java
@@ -0,0 +1,16 @@
+package com.example.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Final_result extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_final_result);
+    }
+
+
+}
