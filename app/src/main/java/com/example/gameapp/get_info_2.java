512801ff32cc3237ffdf88fbf55dbc7b4a811819
Angelina <angelinabommarito@live.fr>
2020-04-17 17:09:17 +0200

    Third page

---
+++ app/src/main/java/com/example/gameapp/get_info_2.java
@@ -0,0 +1,14 @@
+package com.example.gameapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class get_info_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_get_info_2);
+    }
+}
